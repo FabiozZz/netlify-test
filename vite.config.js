9bd408818271f3e8702fb40fc2e4d06ce77907f0
Mr_FabiozZz <mr.fabiozzz@gmail.com>
2023-11-25 21:52:22 +0300

    Fix config

--- vite.config.js
+++ vite.config.js
@@ -88,14 +88,17 @@ export default defineConfig({
     // root: resolve(__dirname,'src'),
     build: {
         outDir: resolve(__dirname, 'dist'),
-        // rollupOptions:{
-        //     input:{
-        //         nav:resolve(__dirname,'index.html'),
-        //         login:resolve(__dirname,'index.html'),
-        //         signIn:resolve(__dirname,'index.html'),
-        //     },
-        //
-        // }
+        rollupOptions:{
+            input:{
+                nav:resolve(__dirname,'index.html'),
+                login:resolve(__dirname,'src/pages/login/index.html'),
+                signIn:resolve(__dirname,'src/pages/signIn/index.html'),
+                notFound:resolve(__dirname,'src/pages/NotFound/index.html'),
+                serverError:resolve(__dirname,'src/pages/serverError/index.html'),
+                profile:resolve(__dirname,'src/pages/profile/index.html'),
+            },
+
+        }
     },
     plugins: [handlebars({
         partialDirectory: resolve(__dirname, 'src/partials'),
